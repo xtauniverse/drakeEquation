30fe133bda49e7dc9af88b85d8c18885e455fe48
Gabriel Walters <gwalters@lhschools.org>
2020-01-26 23:11:05 -0500

    intellij Commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,26 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static void drakeEquation(){
+/*Drake equation
+N = number of intelligent species that can communicate with us
+R∗ = 1 yr−1 (1 star formed per year, on the average over the life of the galaxy; this was regarded as conservative)
+fp = 0.2 to 0.5 (one fifth to one half of all stars formed will have planets)
+ne = 1 to 5 (stars with planets will have between 1 and 5 planets capable of developing life)
+fl = 1 (100% of these planets will develop life)
+fi = 1 (100% of which will develop intelligent life)
+fc = 0.1 to 0.2 (10–20% of which will be able to communicate)
+L = 1000 to 100,000,000 years (which will last somewhere between 1000 and 100,000,000 years)
+ */
+
+
+
+    }
+
+}
